3eaaab7f0763e1b08dc154b0790097530fe4a985
Ntokozo Shagala <ntokozog0@gmail.com>
2016-12-06 14:47:57 +0200

    Initial commit

---
+++ FSharpProblems/FSharpProblems.fs
@@ -0,0 +1,57 @@
+﻿namespace FSharpProblems
+
+module Problem1 = 
+    let MyLast elements =  
+        elements
+          |> Seq.last
+
+    let MyLast2 elements =  
+        elements
+        |> Seq.rev
+        |> Seq.head
+
+    let rec MyLast3 elements =  
+        match elements with
+            | [] -> failwith "need a non-empty list"
+            | [x] -> x
+            | _::tail -> MyLast3 tail
+
+    let MyLast4 elements =  
+        List.reduce (fun _ b -> b) elements
+
+module Problem2 = 
+    let myButLast elements = 
+        elements
+         |> Seq.rev
+         |> Seq.tail
+         |> Seq.head
+
+    let rec myButLast2 elements = 
+         match elements with
+            | [] -> failwith "need a non-empty list"
+            | [x] -> failwith "need a list with at least two items"
+            | [x;y] -> x
+            | _::tail -> myButLast2 tail
+
+    let myButLast3 (elements : list<int>) = 
+        let lengthOfSecondLast = elements.Length - 2;
+        elements.Item(lengthOfSecondLast)
+
+    let myButLast4 (elements : list<int>) = 
+        elements.[elements.Length - 2]
+
+    let myButLast5 elements = 
+        elements
+        |> List.rev
+        |> List.item 1
+
+module Problem3 = 
+    let elementAt elements position = 
+        elements
+        |> List.item(position - 1)
+        
+    let rec elementAt2 elements position = 
+        match elements, position with
+        | [],_ -> failwith "error"
+        | head::tail, 1 -> head
+        | head::tail, n -> elementAt2 tail (n-1)
---
+++ TestFSharpProblems/FsharpTests.fs
@@ -0,0 +1,122 @@
+﻿namespace TestFSharpProblems
+
+open FsUnit
+open NUnit.Framework
+open FSharpProblems
+
+
+[<TestFixture>]
+type Problem1Tests() = 
+            
+    [<Test>]
+    member this.``Given list with single item should return item``() = 
+        [5]
+        |> Problem1.MyLast
+        |> should equal 5
+
+    [<Test>]
+    member this.``Given list with multiple items should return last item``() = 
+        [5;6;7]
+        |> Problem1.MyLast
+        |> should equal 7
+
+            
+    [<Test>]
+    member this.``2 Given list with single item should return item``() = 
+        [5]
+        |> Problem1.MyLast2
+        |> should equal 5
+
+    [<Test>]
+    member this.``2 Given list with multiple items should return last item``() = 
+        [5;6;7]
+        |> Problem1.MyLast2
+        |> should equal 7
+
+    [<Test>]
+    member this.``3 Given list with single item should return item``() = 
+        [5]
+        |> Problem1.MyLast3
+        |> should equal 5
+
+    [<Test>]
+    member this.``3 Given list with multiple items should return last item``() = 
+        [5;6;7]
+        |> Problem1.MyLast3
+        |> should equal 7
+
+    [<Test>]
+    member this.``4 Given list with single item should return item``() = 
+        [5]
+        |> Problem1.MyLast4
+        |> should equal 5
+
+    [<Test>]
+    member this.``4 Given list with multiple items should return last item``() = 
+        [5;6;7]
+        |> Problem1.MyLast4
+        |> should equal 7
+
+
+[<TestFixture>]
+type Problem2Tests() =
+    [<Test>]
+    member this.``Given list with two items should return first item``() = 
+        [6;7]
+        |> Problem2.myButLast
+        |> should equal 6
+        
+    [<Test>]
+    member this.``Given list with multiple items should return second last item``() = 
+        [6;7;5;8]
+        |> Problem2.myButLast
+        |> should equal 5 
+        
+        
+    [<Test>]
+    member this.``2 Given list with multiple items should return second last item``() = 
+        [6;7;5;8]
+        |> Problem2.myButLast2
+        |> should equal 5
+
+        
+    [<Test>]
+    member this.``3 Given list with multiple items should return second last item``() = 
+        [6;7;5;8;123;543]
+        |> Problem2.myButLast3
+        |> should equal 123  
+
+    [<Test>]
+    member this.``4 Given list with multiple items should return second last item``() = 
+        [6;7;5;8;123;543]
+        |> Problem2.myButLast4
+        |> should equal 123
+
+    [<Test>]
+    member this.``5 Given list with multiple items should return second last item``() = 
+        [6;7;5;8;123;543]
+        |> Problem2.myButLast5
+        |> should equal 123
+
+[<TestFixture>]
+type Problem3Tests() =
+    [<Test>]
+    member this.``Given list with three items and K of 2 should return second item``() = 
+        let elements = [1;2;3]
+        let result = Problem3.elementAt elements 2
+        result 
+            |> should equal 2  
+            
+    [<Test>]
+    member this.``Given a list of strings should return matching character``() = 
+        let elements = (List.ofSeq "fsharp")
+        let result = Problem3.elementAt elements 5
+        result 
+            |> should equal 'r'   
+                     
+    [<Test>]
+    member this.``2 Given a list of strings should return matching character``() = 
+        let elements = (List.ofSeq "fsharp")
+        let result = Problem3.elementAt2 elements 5
+        result 
+            |> should equal 'r'
